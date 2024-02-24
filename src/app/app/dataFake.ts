45b8f88d923d4abe6da5d54356f045a7b51e5dec
Joao Pedro <j.pedroo2000@hotmail.com>
2024-02-23 22:47:04 -0300

    Content created

---
+++ src/app/app/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover": "https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png",
+    "title": "Saiu novo filme",
+    "description": "Simsalabim",
+  },
+  {
+    "id":"2",
+    "photoCover": "https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png",
+    "title": "Saiu novo filmeee",
+    "description": "Simsalabim",
+  },
+  {
+    "id":"3",
+    "photoCover": "https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png",
+    "title": "Saiu novo filmeeeee",
+    "description": "Simsalabimmm",
+  },
+  {
+    "id":"4",
+    "photoCover": "https://cordierinvestimentos.com.br/wp-content/uploads/2020/09/placeholder.png",
+    "title": "Saiu novo filmeeeee",
+    "description": "Simsalabimmm",
+  }
+]
